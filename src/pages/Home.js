d7c1b56268bbd9105932503cf574375563110c89
AndrewYoung72 <97897877+AndrewYoung72@users.noreply.github.com>
2022-08-17 20:45:21 -0600

    header work/login btn with styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,12 +6,15 @@ export default function Home(){
   return(
     <div className="grid">
       <div className="">
-        <nav>
-          <div className="border-b-2 border-primary">
-            <h1  className=" text-gray-600 font-bold text-6xl text-center font-serif">See Food</h1>
-            <a href="/login" className=" text-j">Login</a>
+        <nav className=" text-end">
+          <div className="border-b-2 bg-slate-200 border-primary py-2">
+            <h1  className=" text-primary underline font-bold text-6xl text-center font-serif p-4">See Food</h1>
+            <button className=" btn border-4 hover:border-primary transform hover:scale-125 bg-white ">Login</button>
           </div>
         </nav>
+      </div>
+      <div className=" py-6 bg-gray-100 ">
+
       </div>
     </div>
   )
